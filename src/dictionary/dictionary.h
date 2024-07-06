a88489f2f7bb97fb6175892c52711bb8843f61de
Cory Coward <ccoward80@gmail.com>
2024-07-05 23:49:04 -0400

    Add documentation to dictionary library

--- src/dictionary/dictionary.h
+++ src/dictionary/dictionary.h
@@ -7,33 +7,96 @@
 #define FNV_OFFSET          14695981039346656037UL
 #define FNV_PRIME           1099511628211UL
 
+/**
+ * @brief A struct representing a key/value pair in the dictionary.
+ * 
+ */
 typedef struct
 {
-    const char *key;
-    void *value;
+    const char *key;    /**< Key as a string. */
+    void *value;        /**< Value as a void pointer. */
 } DictionaryEntry;
 
+/**
+ * @brief Struct representing a dictionary.
+ * 
+ */
 typedef struct 
 {
-    DictionaryEntry *entries;
-    size_t capacity;
-    size_t length;
+    DictionaryEntry *entries;   /**< Array of dictionary items. */
+    size_t capacity;            /**< Maximum capacity of the dictionary. */
+    size_t length;              /**< Current number of items in the dictionary. */
 } Dictionary;
 
+/**
+ * @brief A struct representing an iterator over a dictionary.
+ * 
+ */
 typedef struct
 {
-    const char *key;
-    void *value;
-    Dictionary *_dict;
-    size_t _index;
+    const char *key;        /**< The current key. */
+    void *value;            /**< The current value. */
+    Dictionary *_dict;      /**< The dictionary for the current iterator. */
+    size_t _index;          /**< The current index of the iterator. */
 } DictionaryIterator;
 
+/**
+ * @brief Initialize a new dictionary.
+ * 
+ * @return A pointer to the newly created dictionary.
+ */
 Dictionary *dict_create(void);
+
+/**
+ * @brief Destroys a dictionary instance and frees all allocated memory.
+ * 
+ * @param dict A pointer to the dictionary to destroy.
+ */
 void dict_destroy(Dictionary *dict);
+
+/**
+ * @brief Retrieves value associated with key.
+ * 
+ * @param dict A pointer to the dictionary.
+ * @param key The key as a string.
+ * @return A pointer to the value if the key is found, NULL otherwise.
+ */
 void *dict_get(Dictionary *dict, const char *key);
+
+/**
+ * @brief Add a new key/value pair to the dictionary.
+ * 
+ * @param dict A pointer to the dictionary.
+ * @param key The key to add, as a string.
+ * @param value The value to add, as a void pointer.
+ * @return The newly added key, as a string.
+ */
 const char *dict_add_kv(Dictionary *dict, const char *key, void *value);
+
+/**
+ * @brief Add a new DictionaryEntry to the dictionary.
+ * 
+ * @param dict A pointer to the dictionary.
+ * @param entry A pointer to the DictionaryEntry to add.
+ * @return The newly added key, as a string.
+ */
 const char *dict_add_entry(Dictionary *dict, DictionaryEntry *entry);
+
+/**
+ * @brief Checks if the dictionary contains a specified key.
+ * 
+ * @param dict A pointer to the dictionary.
+ * @param key The key to search for, as a string.
+ * @return True if the key was found, false otherwise.
+ */
 bool dict_contains_key(Dictionary *dict, const char *key);
+
+/**
+ * @brief Returns the current size of the dictionary.
+ * 
+ * @param dict A pointer to the dictionary.
+ * @return The current size of the dictionary.
+ */
 size_t dict_length(Dictionary *dict);
 
 #endif // DICTIONARY_H
