5d0483f744955d027ff99fe3b5fa2641be59a8f1
Cory Coward <ccoward80@gmail.com>
2024-07-09 22:45:08 -0400

    Set up basic TCP connection

--- src/server/server.c
+++ src/server/server.c
@@ -1,4 +1,9 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 #include "server.h"
 #include "dictionary.h"
 
@@ -40,5 +45,54 @@ const char *get_mime_type(const char *f_ext)
 
 const char *start_server(void)
 {
-    return "Server starting...";
+    char *ip = "127.0.0.1";
+    in_port_t port = 9999;
+
+    int server_sock, client_sock, n;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_size;
+    char buffer[1024];
+
+    server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock < 0)
+    {
+        perror("Socket error");
+        exit(EXIT_FAILURE);
+    }
+    printf("TCP server socket created.\n");
+
+    memset(&server_addr, '\0', sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = port;
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+
+    n = bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (n < 0)
+    {
+        perror("Socket bind error");
+        exit(EXIT_FAILURE);
+    }
+    printf("Bind to port %d\n", port);
+
+    listen(server_sock, 5);
+    printf("Listening...\n");
+
+    while (true)
+    {
+        addr_size = sizeof(client_addr);
+        client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &addr_size);
+        printf("Client connected.\n");
+
+        bzero(buffer, 1024);
+        recv(client_sock, buffer, sizeof(buffer), 0);
+        printf("Client: %s\n", buffer);
+
+        bzero(buffer, 1024);
+        strncpy(buffer, "SERVER ACK", 1024);
+        printf("Server: %s\n", buffer);
+        send(client_sock, buffer, strlen(buffer), 0);
+
+        close(client_sock);
+        printf("Client disconnected.");
+    }
 }
