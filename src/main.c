f3a5ba01386e1a3e51a1b41a338860441944460b
Cory Coward <cory@c2softwaresolutions.com>
2024-07-12 15:35:55 -0400

    Add sample request data

--- src/main.c
+++ src/main.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "requests/requests.h"
+#include "file_utilities/file_utilities.h"
 
 int main(void)
 {
-    char req[50] = "GET /blog/posts/1234 HTTP/1.1";
-    HttpRequestDetails details;
-    if (parse_request(req, &details) == 0)
+    const char *fn = "../data/sample_header.txt";
+    TextFile text_file;
+
+    if (read_text_file(fn, &text_file) == 0)
     {
-        printf("HTTP method: %s\n", details.request_type);
-        printf("Path: %s\n", details.path);
-        printf("Version: %d--%d\n", details.version.ver_major, details.version.ver_minor);
+        HttpRequestDetails details;
+        if (parse_request(text_file.lines[0], &details) == 0)
+        {
+            printf("HTTP method: %s\n", details.request_type);
+            printf("Path: %s\n", details.path);
+            printf("Version: %d.%d\n", details.version.ver_major, details.version.ver_minor);
+        }
+
+        free_text_file(&text_file);
     }
     return EXIT_SUCCESS;
 }
